d0fa47e68af6ed74ce89034667101e6cd928e91a
ayorich <kayodele17@yahoo.com>
2020-02-23 10:53:48 +0100

    count unique values

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-
-// solution one leads to O(N**2)
-function sumZero(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === 0) {
-                return [arr[i], arr[j]];
-            }
+function countUniqueValues(arr) {
+    if (arr.length === 0) return 0;
+    var i = 0;
+    for (var j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
         }
+        
     }
+    return i + 1;
 }
-
-
-const result = sumZero([-4, -3, -2, -1, 0, 1, 2, 5])
-console.log(result)
-
-
-//BEST SOLUTION 
-function sumZero(arr){
-    let left = 0;
-    let right = arr.length - 1;
-    while(left < right){
-        let sum = arr[left] + arr[right];
-        if(sum === 0){
-            return [arr[left], arr[right]];
-        }else if(sum > 0){
-            right--;
-        }else{
-            left++;
-        }
-    }
-}
-
-const bestResult = sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 5]);
-console.log(bestResult);
+const result = countUniqueValues([1, 2, 2, 5, 7, 7, 99]);
+console.log(result);
