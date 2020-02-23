42c68a470fbdb3cfe34a6df50fb03a79003230f5
ayorich <kayodele17@yahoo.com>
2020-02-23 11:12:54 +0100

    sliding window

--- README.txt
+++ README.txt
@@ -30,3 +30,23 @@ function maxSubarraySum(arr, num) {
 }
 
  maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
+
+
+ probably best solution will result in O(2N) = O(2)
+
+ function maxSubarraySum(arr, num){
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function maxSubarraySum(arr, num) {
 
 const result = maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
 console.log(result)
+
+
+//best solution 
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+const bestSolution = maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)
+console.log(bestSolution)
