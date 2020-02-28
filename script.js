b960219a276165d628b3128d1b5ed2cfb4d51f61
ayorich <kayodele17@yahoo.com>
2020-02-28 12:24:13 +0100

    callstacking process

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-//solution one 
-// TIME COMPLEXITY O(N) -LINEAR SEARCH
-function searchFirst(arr,val){
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === val){
-            return i;
-        }
-    }
-    return -1;
+function takeShower(){
+    return "Showering!"
 }
-const result = searchFirst([2,3,4,6,7,8,9,11,12,14,16,17,19,30,35,45,47,56,78,87,], 35)
-console.log(result);
-//best solution 
-// TIME COMPLEXITY LOG(N) -BINARY SEARCH
-function search(array, val){
-    let min = 0 ;
-    let max = array.length-1;
-    while(min <= max){
-        let middle = Math.floor((min +max) /2);
-        let currentElement = array[middle];
-        if (array[middle] < val ){
-            min = middle + 1;
-        }else if (array[middle] > val){
-            max = middle -1;
-        }else{
-            return [middle, currentElement];
-        }
-    }
-    return -1;
+
+function eatBreakfast(){
+    let meal = cookFood()
+    return `Eating ${meal}`
 }
-                    //     0  1  2  3  4  5  6  7   8                    
-const bestResult = search([2, 3, 4, 6, 7, 8, 9, 11, 12], 9)
-console.log(bestResult);
+
+function cookFood(){
+    let items = ["Oatmeal", "Eggs", "Protein Shake"]
+    return items[Math.floor(Math.random()*items.length)];
+}
+function wakeUp() {
+    takeShower()
+    eatBreakfast()
+    console.log("Ok ready to go to work!")
+}
+
+wakeUp()
