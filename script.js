e1be7a31281e60960c24a11bcecd02d7b5bd0867
ayorich <kayodele17@yahoo.com>
2020-03-20 16:59:00 +0100

    tree-traversal

--- script.js
+++ script.js
@@ -65,7 +65,21 @@ class BinarySearchTree {
         }
         return false;
     }
-   
+    //BREADTH FIRST TREE SEARCH
+    BFS() {
+        var node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
             // 10
         //  6       15
