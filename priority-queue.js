e02bfd4cc85d89cba236f61db0cc94b7a8545af8
ayorich <kayodele17@yahoo.com>
2020-03-21 13:34:39 +0100

    priority-queue

--- priority-queue.js
+++ priority-queue.js
@@ -8,19 +8,21 @@ class PriorityQueue {
         this.bubbleUp();
     }
     bubbleUp() {
-        let idx = this.values.length - 1;
-        const element = this.values[idx];
+        let idx = this.values.length - 1;//4
+        const element = this.values[idx];//p3
         while (idx > 0) {
-            let parentIdx = Math.floor((idx - 1) / 2);
-            let parent = this.values[parentIdx];
+            let parentIdx = Math.floor((idx - 1) / 2);//1
+            let parent = this.values[parentIdx];//p2
             if (element.priority >= parent.priority) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent;
             idx = parentIdx;
         }
     }
-   
-}
+    print(){
+        return this.values;
+    }
+    }
 class Node {
     constructor(val, priority) {
         this.val = val;
@@ -28,8 +30,11 @@ class Node {
     }
 }
 let ER = new PriorityQueue();
-ER.enqueue("common cold", 5)
+ER.enqueue("common cold", 5) 
 ER.enqueue("gunshot wound", 1)
 ER.enqueue("high fever", 4)
 ER.enqueue("broken arm", 2)
 ER.enqueue("glass in foot", 3)
+console.log(ER.print())
+//enqueue => [5,1,4,2,3]
+// result=>[1,2,4,5,3]
--- script.js
+++ script.js
@@ -35,3 +35,45 @@ heap.insert(55);
 console.log(heap.print())
 // insert => [41,39,33,18,27,12,55]
 // result => [55,39,41,18,27,12,33]
+
+//-------------PRIORITY QUEUE----------//
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1;//4
+        const element = this.values[idx];//p3
+        while (idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2);//1
+            let parent = this.values[parentIdx];//p2
+            if (element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    print() {
+        return this.values.map(el => el.priority);
+    }
+}
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+let ER = new PriorityQueue();
+ER.enqueue("common cold", 5)
+ER.enqueue("gunshot wound", 1)
+ER.enqueue("high fever", 4)
+ER.enqueue("broken arm", 2)
+ER.enqueue("glass in foot", 3)
+console.log(ER.print())
+//enqueue => [5,1,4,2,3]
+// result=>[1,2,4,5,3]
