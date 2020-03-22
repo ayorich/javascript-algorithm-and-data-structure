83c4ae854463c7504e432d677d2729a9e0516149
ayorich <kayodele17@yahoo.com>
2020-03-22 15:54:12 +0100

    graph add edge

--- graph_add_vertex.js
+++ graph_add_vertex.js
@@ -5,8 +5,4 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
-    addEdge(v1, v2) {
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
 }
