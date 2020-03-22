0a2650d286c32e2c613043e7cfa0e8d3cfab5881
ayorich <kayodele17@yahoo.com>
2020-03-22 16:11:58 +0100

    graph depth first recursive

--- graph_DFS_recursive.js
+++ graph_DFS_recursive.js
@@ -24,6 +24,24 @@ class Graph {
         }
         delete this.adjacencyList[vertex]
     }
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
     
 }
 
@@ -46,6 +64,7 @@ g.addEdge("C", "E")
 g.addEdge("D", "E")
 g.addEdge("D", "F")
 g.addEdge("E", "F")
+g.depthFirstRecursive("A")
 
 //          A
 //        /   \
