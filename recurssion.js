24187bb42558162875e0b8b477c30878ce2ef8db
ayorich <kayodele17@yahoo.com>
2024-02-24 06:48:50 +0100

    recursion-stack

--- recurssion.js
+++ recurssion.js
@@ -1,4 +1,4 @@
-//sorting array
+//-------------------------sorting array--------------------
 // [9,2,3,1,6]
 //1. call sort function
 //2. pop each values and call sort of remaining array
@@ -34,3 +34,55 @@ const main = () => {
 };
 
 main();
+
+// -------------sort stack-------------
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(data) {
+    this.items.push(data);
+  }
+
+  printStack() {
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      console.log(this.items[i]);
+    }
+  }
+
+  sort() {
+    if (this.items.length === 1) {
+      return this;
+    }
+    const temp = this.items.pop();
+    this.sort();
+    this.insert(temp);
+    return this;
+  }
+
+  insert(temp) {
+    if (this.items.length === 0 || temp >= this.items[this.items.length - 1]) {
+      this.items.push(temp);
+      return;
+    }
+
+    const val = this.items.pop();
+    this.insert(temp);
+    this.items.push(val);
+  }
+}
+
+const stack = new Stack();
+
+stack.push(2);
+stack.push(5);
+stack.push(1);
+stack.push(3);
+
+stack.printStack();
+
+stack.sort();
+
+stack.printStack();
