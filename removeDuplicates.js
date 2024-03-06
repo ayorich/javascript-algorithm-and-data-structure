57d874269770f1603f9c59af4f5e389ed38f671f
ayorich <kayodele17@yahoo.com>
2024-03-06 10:53:42 +0100

    leetcode-lcs-merge-sort-list-remove-duplicate

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -18,19 +18,19 @@ let length = removeDuplicates(nums);
 console.log('Length :', length);
 console.log('Array :', nums.slice(0, length));
 
-[1, 1, 2, 2, 3, 4, 4, 4, 5]
-    ij
-    j  i
-[1, 2, 2, 2, 3, 4, 4, 4, 5]    
-       j  i  
-       j     i
-[1, 2, 3, 2, 3, 4, 4, 4, 5]
-          j     i
-[1, 2, 3, 4, 3, 4, 4, 4, 5]
-             j     i
-             j        i
-             j           i
-[1, 2, 3, 4, 5, 4, 4, 4, 5]
-              j
+// [1, 1, 2, 2, 3, 4, 4, 4, 5]
+//     ij
+//     j  i
+// [1, 2, 2, 2, 3, 4, 4, 4, 5]
+//        j  i
+//        j     i
+// [1, 2, 3, 2, 3, 4, 4, 4, 5]
+//           j     i
+// [1, 2, 3, 4, 3, 4, 4, 4, 5]
+//              j     i
+//              j        i
+//              j           i
+// [1, 2, 3, 4, 5, 4, 4, 4, 5]
+//               j
 
-              return j
+//               return j
